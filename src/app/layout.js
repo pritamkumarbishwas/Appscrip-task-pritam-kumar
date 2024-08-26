c2a61a434d3f7aeaff5e264db8f4911920d7ba06
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-08-26 15:53:47 +0530

    seo setting

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ margin:0,paddign:0 }}>
+      <body className={inter.className} style={{ margin: 0, paddign: 0 }}>
         <Header />
         <Menu />
         {children}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import Products from "@/components/Products";
 import styles from "./page.module.css";
+import Head from 'next/head';
 
 // The Home component is a Server Component in the `app` directory
 export default async function Home() {
@@ -11,14 +12,35 @@ export default async function Home() {
     products = await response.json();
   } catch (error) {
     console.error('Error fetching products:', error);
-    products = []; // Fallback to an empty array if there's an error
+    products = []; 
   }
 
   return (
-    <main className={styles.main}>
-      <HeroSection />
-      {/* Pass the products directly to the Products component */}
-      <Products initialProducts={products} />
-    </main>
+    <>
+      <Head>
+        <title>Shop the Best Products Online | FakeStore</title>
+        <meta
+          name="description"
+          content="Browse through a wide selection of top-quality products at FakeStore. Find great deals on electronics, fashion, and more."
+        />
+        <meta
+          property="og:title"
+          content="Shop the Best Products Online | FakeStore"
+        />
+        <meta
+          property="og:description"
+          content="Browse through a wide selection of top-quality products at FakeStore. Find great deals on electronics, fashion, and more."
+        />
+        <meta
+          property="og:url"
+          content="https://appscrip-task-assignment.netlify.app"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}>
+        <HeroSection />
+        <Products initialProducts={products} />
+      </main>
+    </>
   );
 }
